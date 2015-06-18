4a4e88692cf3ff9d4dbe0c19b75bd54744568b6e
Michael <mikesims321@gmail.com>
2015-06-17 20:28:03 -0700

    Files to Folders.

---
+++ scripts.py/solveMe1.py
@@ -0,0 +1,7 @@
+def solveMeFirst(a,b): # a function that takes two variables and adds them
+  return a+b
+
+num1 = input() # takes one number from user
+num2 = input() # takes another number from user
+res = solveMeFirst(num1,num2) # assigns number to res
+print res # displays sum
