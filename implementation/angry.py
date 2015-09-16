bf6696e4f637e09aa2289ae51d790f85ffe9eee6
Michael <mikesims321@gmail.com>
2015-09-16 15:08:39 -0700

    Update angry.py

--- implementation/angry.py
+++ implementation/angry.py
@@ -1,19 +1,22 @@
-T = input()
-arr = []
-for i in range(T):
-	N = map(int, raw_input().split())
-	limit = N[1]
-	arrivals = map(int, raw_input().split())
-	count = 0
-	for j in arrivals:
-		if j <= 0:
-			count += 1
+T = input() # number of test cases
+cancel = [] # empty array cancel to later store responses
 
-	if count >= limit:
-		arr.append('No')
+for i in range(T): # for however many test cases
 
-	else:
-		arr.append('Yes')
+	N = map(int, raw_input().split()) # store two space-separated integers
+	limit = N[1] # store how many students it takes to keep the professor from canceling class
+	arrivals = map(int, raw_input().split()) # arrivals of students on time (a_i <= 0) or late (a_i > 0) 
+	count = 0 # intialize count to 0
+	
+	for j in arrivals: # for every studnet arrival
+		if j <= 0: # if the student is on time
+			count += 1 # count the number of ontime students
 
-for k in arr:
-	print k
+	if count >= limit: # case where professor doesn't cancel class 
+		cancel.append('NO') # update response to cancel
+
+	else: # case where professor does cancel class
+		cancel.append('YES') # update response to cancel
+
+for k in cancel: # for every response in cancel
+	print k # prnit each response on a new line without ""
